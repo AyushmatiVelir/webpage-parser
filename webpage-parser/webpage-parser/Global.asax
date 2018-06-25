5001c653c1ff72f48f369d83bfa3b5d2b136f390
AyushmatiVelir <ayushmati.das@velir.com>
2018-06-25 09:35:10 -0400

    Set up new MVC solution

---
+++ webpage-parser/webpage-parser/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="webpage_parser.MvcApplication" Language="C#" %>
---
+++ webpage-parser/webpage-parser/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(webpage_parser.Startup))]
+namespace webpage_parser
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
