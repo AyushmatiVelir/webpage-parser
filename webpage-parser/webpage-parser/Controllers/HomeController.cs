db2ba4c5f10d6efe27cff9411e490aac07ab2478
AyushmatiVelir <ayushmati.das@velir.com>
2018-06-25 10:19:31 -0400

    Create new action

--- webpage-parser/webpage-parser/Controllers/HomeController.cs
+++ webpage-parser/webpage-parser/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using webpage_parser.Models;
 
 namespace webpage_parser.Controllers
 {
@@ -25,6 +26,19 @@ namespace webpage_parser.Controllers
 			ViewBag.Message = "Your contact page.";
 
 			return View();
+		}
+
+		
+		[HttpPost]
+		public ActionResult Parse(ParserModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				//TODO: SubscribeUser(model.Email);
+			}
+
+			//return View("Index", model);
+			return View("Contact");
 		}
 	}
 }
---
+++ webpage-parser/webpage-parser/Models/ParserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace webpage_parser.Models
+{
+	public class ParserModel
+	{
+		[Required]
+		public string Url { get; set; }
+	}
+}
